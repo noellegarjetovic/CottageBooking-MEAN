cd090d43e735c372c24621e121a409d74607ab0a
Noelle Garjetović <noelle@gedra.co.rs>
2026-01-07 20:13:23 +0100

    final

--- piaPredaja/backend/src/models/admins.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const adminSchema=new mongoose.Schema({
-    korime:String,
-    lozinka:String
-})
-
-export default mongoose.model('AdminModel',adminSchema,'admins')
